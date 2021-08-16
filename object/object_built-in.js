8d5cbbff5efa21214a61676e3856749d97844767
bjl0615 <bjl0615@gmail.com>
2021-08-16 20:07:30 +0900

    object_built-in

---
+++ object/object_built-in.js
@@ -0,0 +1,13 @@
+/*
+    자바스크립트는 미리 정의된 여러가지 기능을 제공한다.
+    날짜와 관련되 기능, 수학과 관련된 기능 등 여러가지 기능들이 존재한다.
+    이러한 기능들을 잘 정돈하기 위해서 자바스크립트 개발자들은 객체를 이용하기로 했다.
+    예를 들어 Math라는 객체에는 수학과 관련된 여러 함수들이 그룹화되어 있다.
+        console.log("Math.PI", Math.PI) //파이 값을 출력을 해준다.
+        console.log("Math.randon()", Math.random()); //랜덤 값을 출력한다.
+        console.log("Math.floor(3.9), Math.floor(3.9)") //값을 반올림 한다.
+*/
+
+console.log("Math.PI", Math.PI);
+console.log("Math.random()",Math.random());
+console.log("Math.floor(3.9)", Math.floor(3.9));
