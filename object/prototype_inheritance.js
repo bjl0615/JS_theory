550b3123abc8c983dc640698c196e0c7d29e8c50
bjl0615 <bjl0615@gmail.com>
2021-08-19 23:05:23 +0900

    prototype_inheritance

--- object/prototype_inheritance.js
+++ object/prototype_inheritance.js
@@ -47,12 +47,80 @@
             console.log('superObj.superVal => ',superObj.superVal);
             //superObj.superVal => super
 
+        __proto__를 사용해 객체를 상속받는 객체를 만들고 메소드를 추가해보자.
+            kim = {
+                name : 'kim',
+                first:10, second:20,
+                sum:function(){return this.first+this.second}
+            }
+
+            lee = {
+                name:'lee',
+                first:10,second:10,
+                avg:function(){
+                    return(this.first+this.second)/2;
+                }
+            }
+            lee.__proto__ = kim;
+            console.log('lee.sum()', lee.sum());
+            console.log('lee.avg()', lee.avg());
+        같은 내용을 object.create()를 사용해 만들어보자.
+            kim{
+                name:'kim',
+                first:10,second:20,
+                sum:function(){return this.first+this.second}
+            }
+
+            var lee = Object.create(kim);
+            lee.name = 'lee';
+            lee.first = 10;
+            lee.first = 10;
+            lee.avg = function(){
+                return(this.first+this.second)/2;
+            }
+
+            lee.__proto__ = kim;
+            console.log('lee.sum()', lee.sum());
+            console.log('lee.avg()', lee.avg());
+
+
+
 */
 
 var superObj = {superVal : 'super'}
-var subObj = {subVal : 'sub'}
-subObj.__proto__ = superObj;
+// var subObj = {subVal : 'sub'}
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+superObj.subVal = 'sub';
+debugger;
 console.log('subObj.subVal => ', subObj.subVal); 
 console.log('subObj.superVal => ', subObj.superVal); 
 subObj.superVal = 'sub';
 console.log('superObj.superVal => ', superObj.superVal); 
+
+
+var kim = {
+    name : 'kim',
+    fitst : 10,second:20,
+    sum:function(){return this.fitst+this.second}
+}
+
+// var lee = {
+//     name:'lee',
+//     first:10,second:10,
+//     avg:function(){
+//         return (this.first+this.second)/2
+//     }
+// }
+// lee.__proto__ = kim;
+
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function(){
+    return (this.first+this.second)/2;
+}
+console.log('lee.sum() :', lee.sum());
+console.log('lee.avg() :', lee.avg());
+
