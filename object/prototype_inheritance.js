e160827ad1483e8b4b8cd6cf496f6f7c606cd334
bjl0615 <bjl0615@gmail.com>
2021-08-21 20:46:26 +0900

    object_inheritance

--- object/prototype_inheritance.js
+++ object/prototype_inheritance.js
@@ -84,7 +84,33 @@
             console.log('lee.avg()', lee.avg());
 
 
+        기존의 class를 이용한 코드를 다시 살펴보자
+            class Perosn{
+                constructor(name,first,second){
+                    this.name = name;
+                    this.first = first;
+                    this.second = second;
+                }
+                sum(){
+                    return this.first+this.second;
+                }
+            }
+            class PersonPlus extends Person{
+                constructor(name.first.second.third){
+                    super(name,first,second);
+                    this.third = third;
+                }
+                sum(){
+                    return super.sum()+this.third;
+                }
+                avg(){
+                    return(this.first+this.second+this.third)/3;
+                }
+            }
 
+            var kim = new PersonPlus('kim',10,20,30);
+            console.log("kim.sum()",kim.sum());
+            console.log("kim.avg()",kim.avg());
 */
 
 var superObj = {superVal : 'super'}
