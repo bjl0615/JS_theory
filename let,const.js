92b30be1232066c786ad281f20d001ac4f158e99
bjl0615 <bjl0615@gmail.com>
2021-09-23 02:41:19 +0900

    let,const

--- let,const.js
+++ let,const.js
@@ -55,3 +55,82 @@ point();
             - "use strict"가 디폴트 생성
             - 전체는 아님
 */
+
+/*
+    function 블록
+        - function name() {}도 블록 스코프
+        - function 안과 밖에
+            - 같은 이름의 let 변수 선언 가능
+            - 스코프가 다르기 때문
+*/
+let sports = "축구";
+function show(){
+    let sports = "농구";
+    log("안: ", sports);
+};
+show();
+log("밖: ", sports);
+/*
+        - function 밖의 let 변수를
+            - function 안에서 사용 가능(클로저)
+*/     
+let sports = "축구";
+function show(){
+    log(sports);
+};
+show();
+/*
+    try-catch
+        - try-catch 문도 블록 스코프
+        - try 블록{} 기준으로
+            - 안과 밖에 같은 이름의 let 변수 선언 가능
+*/
+let sports = "축구";
+try{
+    let sports = "농구";
+    log("안 : ", sports);
+} catch(e){};
+log("밖: ", sports);
+
+// 1. try 블록의 안과 밖에 let sports를 선언했으며
+// 2. 안과 밖이 스코프가 다르므로 변숫값이 각각 설졍된다.
+
+/*
+        - catch()에서 try 밖의 변수 사용
+*/
+let sports = "축구";
+try{
+    let sports = "농구";
+    log("안 : ", sports);
+    abc= error;
+} catch(e){
+    log("catch: ", sports);
+};
+log("밖: ", sports);
+// 1. let sports = "농구"; try 블록에서 값을 할당한다.
+// 2. abc = error; error 변수가 없으므로 에러가 발생한다.
+// 3. log("catch: ", sports) try 블록 안에서 선언한 sports 값을 출력하지 않고 try 밖의 sports 값을 출력한다.
+
+/*
+    switch-case
+        - switch 문도 블록 스코프
+        - switch 블록 기준으로
+            - 같은 이름의 let 변수 작성 불가
+*/
+let item = 1;
+switch (item) {
+    case 1: 
+        let sportsl
+        break;
+    case 2:
+        // let sports;
+    default:
+        log(sports);
+};
+
+// 1. // let sports;
+// 2. switch 블록 안에서 let을 사용하여 선언한 변수가 있는데 다시 let을 사용하여 변수를 선언하므로 에러가 발생한다.
+// 3. 그래서 주석을 처리했다.
+// 4. 살행에러가 아니라 컴파일 에러
+
+
