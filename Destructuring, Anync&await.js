b3983cc43613032b09d654eff6eecf92bab5c02e
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-11-23 13:09:53 +0900

    Update Async & Await

--- Destructuring, Anync&await.js
+++ Destructuring, Anync&await.js
@@ -43,3 +43,28 @@ console.log(headers);
 */
 var { data : captain } = fetchData();
 console.log(captain);
+
+/*
+    Async & Await
+        어싱크 어웨이트는 자바스크립트 비동기 처리 패턴 중 가장 최근에 나온 문법이다.
+        기존의 비동기 처리 방식인 콜백 함수와 프로미스의 단전을 보완하고 개발자가 읽기 
+        좋은 코드를 작성 할 수 있게 도와준다.
+    
+    async & await 기본 문법
+*/
+async function 함수명() {
+    await 비동기_처리_메서드_명();
+}
+/*
+    먼저 함수의 앞에 async 라는 예약어를 붙인다. 그리고 나서 함수의 내부 로직 중 HTTP 통신을 하는 
+    비동기 처리 코드 앞에 await를 붙인다. 
+    여기서 주의해야할 점은 비동기 처리 메서다그 꼭 프로미스 객체를 반환해야 await가 의도한 대로 동작한다.
+
+    일반적으로 await의 대상이 되는 비동기 처리 코드는 Axios등 프로미스를 반환하는 API 호출 함수이다.
+*/
+function fetchData() {
+    $.ajax('user/1', function(user) {
+        console.log(user);
+    });
+    console.log(user);
+}
